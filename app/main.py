d7edd5a6dd7e6a0d5112d1c9af358bd564589a2a
gabriel guzman <ggtpi0gabriel@gmail.com>
2026-03-21 22:21:59 -0300

    feat: Defined cors
    * middleware CORS w/ params
    * update env
    * update gitignore

--- app/main.py
+++ app/main.py
@@ -1,11 +1,28 @@
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from app.routes import pacientes
 
 load_dotenv()
 
+"""origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://redautoshop.com.ar",
+    "https://www.redautoshop.com.ar",
+] """
+
 app = FastAPI(title=os.getenv("APP_NAME", "Pharma API"))
+
+# cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=os.getenv("ALLOWED_ORIGINS").split(","),
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "PATCH"],
+    allow_headers=["Content-Type", "Authorization"],
+)
 
 # routes
 app.include_router(pacientes.router)
